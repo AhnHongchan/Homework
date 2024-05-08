0c9ab70e1e4ee1fb7db8ed679fcf92fb23871930
AhnHongchan <hha0408@gmail.com>
2024-05-08 18:07:27 +0900

    240508

---
+++ 24_05/240508/vue_hw_4_4/my-first-front-project-mk2/src/components/ColorChanger.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <input type="text" v-model="colorClassInput" @input="changeColor">
+      <p :class="colorClass">입력창에 올바른 색상 명을 입력하면 글자색이 바껴요.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        colorClassInput: '',
+        colorClass: ''
+      };
+    },
+    methods: {
+      changeColor() {
+        if (['red', 'blue', 'green'].includes(this.colorClassInput)) {
+          this.colorClass = this.colorClassInput;
+        } else {
+          this.colorClass = '';
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .red {
+    color: red;
+  }
+  
+  .blue {
+    color: blue;
+  }
+  
+  .green {
+    color: green;
+  }
+  </style>
+  
